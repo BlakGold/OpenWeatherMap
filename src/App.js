262438cd6eeb2468d6fc9dc585ac30b5637c5c47
Ryan E. Hamilton <rhamilton@blakgold.com>
2020-03-15 03:39:24 -0400

    fetch some weather data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from "react";
 import Map from "./components/Map";
+import { weatherApiUrlRoot, apiKey_OpenWeatherMaps } from "./globals/constants";
 import styles from "./App.scss";
 
 class App extends React.Component {
   state = {
+    data: {},
     dataLoaded: false
   };
 
+  async fetchApiData() {
+    const weatherApiUrl = weatherApiUrlRoot + apiKey_OpenWeatherMaps;
+    console.log(`*************************************************`);
+    console.log(`fetching OpenWeatherMap API data: ${weatherApiUrl}`);
+    console.log(`*************************************************`);
+    await fetch(weatherApiUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(`#################################################`);
+        console.log(data);
+        console.log(`#################################################`);
+        this.setState({
+          dataLoaded: true,
+          data
+        });
+      });
+  }
+
   componentDidMount() {
     let apiCallDuration = 3000;
+    console.log(`=================================================`);
     console.log(`*** App: componentDidMount() ***`);
-    console.log(`fetching OpenWeatherMap API data...`);
-    setTimeout(() => {
-      this.setState({
-        dataLoaded: true
-      });
-    }, apiCallDuration);
+    console.log(`-------------------------------------------------`);
+    console.log(this.state);
+    console.log(`-------------------------------------------------`);
+    console.log(`=================================================`);
+    this.fetchApiData();
   }
 
   componentDidUpdate() {
+    console.log(`=================================================`);
     console.log(`*** App: componentDidUpdate() ***`);
+    console.log(`-------------------------------------------------`);
+    console.log(this.state);
+    console.log(`-------------------------------------------------`);
+    console.log(`=================================================`);
   }
 
   render() {
---
+++ src/globals/constants.js
@@ -0,0 +1,5 @@
+const weatherApiUrlRoot =
+  "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=";
+const apiKey_OpenWeatherMaps = "f81d9bcbe8e93533a28127f5134aa81a";
+
+export { apiKey_OpenWeatherMaps, weatherApiUrlRoot };
