31d88d9e02ed652cb67c209d736421c807781d6a
Ryan E. Hamilton <rhamilton@blakgold.com>
2020-03-14 23:55:06 -0400

    some basic font style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>OpenWeatherMap</h1>
+      <p>A demo of OpenWeatherMap API.</p>
+    </div>
+  );
 }
 
 export default App;
