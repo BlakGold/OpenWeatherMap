1dfd4aee53c388ba31e4c3d3bf397f14c3941184
Ryan E. Hamilton <rhamilton@blakgold.com>
2020-03-17 15:39:36 -0400

    filter and sort the Top 50 cities in the US

--- src/components/CitySelectMenu/index.js
+++ src/components/CitySelectMenu/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import cities from "../../data/city.list.json";
+import { US, cityIds_Top50_inUS } from "../../globals/constants";
 import styles from "./index.scss";
 
 function CitySelectMenu(props) {
   const { countryCode, selectedCityId, onChange } = props;
-  const citiesByCountry = cities.filter(city => city.country === countryCode);
+  let filteredCities = cities
+    .filter(city => city.country === countryCode)
+    .sort((city1, city2) => {
+      if (city1.name > city2.name) {
+        return 1;
+      }
+      return -1;
+    });
+  if (countryCode === US) {
+    filteredCities = filteredCities.filter(city =>
+      cityIds_Top50_inUS.includes(city.id)
+    );
+  }
 
   return (
     <select className={styles.menu} value={selectedCityId} onChange={onChange}>
-      {citiesByCountry.map(city => {
+      {filteredCities.map(city => {
         const fullCityName = `${city.name}, ${
           city.state ? `${city.state}, ` : ""
         }${city.country}`;
