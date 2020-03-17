8e90f67d9e66156c97e95183c079951f32820dd1
Ryan E. Hamilton <rhamilton@blakgold.com>
2020-03-17 14:28:53 -0400

    remove console.log from Map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import styles from "./index.scss";
 
 function Map(props) {
-  //console.log(`*************************************************`);
-  //console.log(`fetching OpenWeatherMap API data: ${props.mapUrl}`);
-  //console.log(`*************************************************`);
   return (
     <iframe
       title="OpenWeatherMap"
