9e6c485fd3aee3047533c8dcb1c40d09e8f65820
Ryan E. Hamilton <rhamilton@blakgold.com>
2020-03-18 03:27:22 -0400

    reorder constants

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,3 +1,18 @@
+/* API Config & Endpoints */
+const apiKey_OpenWeatherMaps = "f81d9bcbe8e93533a28127f5134aa81a";
+const currentWeatherApiUrlRoot =
+  "https://api.openweathermap.org/data/2.5/weather"; // ?id=2172797&appid=
+const weatherForecastApiUrlRoot =
+  "https://api.openweathermap.org/data/2.5/forecast"; // ?q=London,us&mode=json&appid=
+//const mapApiUrlRoot = "https://tile.openweathermap.org/map/"; // temp_new/3/7/7.png?appid=
+
+/* Icons */
+const iconField = "{icon}";
+const icon2xField = "@2x";
+const iconUrlTemplate =
+  "http://openweathermap.org/img/wn/" + iconField + "@2x.png";
+
+/* Locations */
 const US = "US";
 const cityIds_Top50_inUS = [
   5128581,
@@ -53,30 +68,15 @@ const cityIds_Top50_inUS = [
 ];
 const cityId_WashingtonDC = 4140963;
 
-const apiKey_OpenWeatherMaps = "f81d9bcbe8e93533a28127f5134aa81a";
-
-//const mapApiUrlRoot = "https://tile.openweathermap.org/map/"; // temp_new/3/7/7.png?appid=
-
-const currentWeatherApiUrlRoot =
-  "https://api.openweathermap.org/data/2.5/weather"; // ?id=2172797&appid=
-
-const weatherForecastApiUrlRoot =
-  "https://api.openweathermap.org/data/2.5/forecast"; // ?q=London,us&mode=json&appid=
-
-const iconField = "{icon}";
-const icon2xField = "@2x";
-const iconUrlTemplate =
-  "http://openweathermap.org/img/wn/" + iconField + "@2x.png";
-
 export {
-  US,
-  cityIds_Top50_inUS,
-  cityId_WashingtonDC,
   apiKey_OpenWeatherMaps,
-  /*mapApiUrlRoot,*/
   currentWeatherApiUrlRoot,
   weatherForecastApiUrlRoot,
+  /*mapApiUrlRoot,*/
   iconField,
   icon2xField,
-  iconUrlTemplate
+  iconUrlTemplate,
+  US,
+  cityIds_Top50_inUS,
+  cityId_WashingtonDC
 };
